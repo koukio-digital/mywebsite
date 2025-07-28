d4cded8666ee4b81053c2d525e2f1dd8bf51e016
F1ankush <ankush4u1811@gmail.com>
2025-07-28 12:49:21 +0530

    change testimonial for fonder voice

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@
 
 
     // Sticky Navbar
-    $(window).scroll(function () {
+      $(window).scroll(function () {
         if ($(this).scrollTop() > 45) {
             $('.navbar').addClass('sticky-top shadow-sm');
         } else {
